6d5e86c4485847d6841e4b7eed47e2063a9f9b79
hottinggot <hottinggot@naver.com>
2021-02-01 02:04:14 -0800

    add scheduling algorithm rr & priority

--- kernel/task.c
+++ kernel/task.c
@@ -4,6 +4,10 @@
 
 static KernelTcb_t sTask_list[MAX_TASK_NUM];
 static uint32_t sAllocated_tcb_index;
+static uint32_t sCurrent_tcb_index;
+
+static KernelTcb_t* Scheduler_round_robin_algorithm(void);
+static KernelTcb_t* Scheduler_priority_algorithm(void);
 
 void Kernel_task_init(void)
 {
@@ -21,7 +25,7 @@ void Kernel_task_init(void)
 	}
 }
 
-uint32_t Kernel_task_create(KernelTaskFunc_t startFunc)
+uint32_t Kernel_task_create(KernelTaskFunc_t startFunc, uint32_t priority)
 {
 	KernelTcb_t* new_tcb = &sTask_list[sAllocated_tcb_index++];
 
@@ -29,8 +33,36 @@ uint32_t Kernel_task_create(KernelTaskFunc_t startFunc)
 		return NOT_ENOUGH_TASK_NUM;
 	}
 
+	new_tcb->priority = priority;
+
 	KernelTaskContext_t* ctx = (KernelTaskContext_t*)new_tcb->sp;
 	ctx->pc = (uint32_t)statrFunc;
 
 	return sAllocated_tcb_index-1;
 }
+
+static KernelTcb_t* Scheduler_round_robin_algorithm(void)
+{
+	sCurrent_tcb_index++;
+	sCurrent_tcb_index %= sAllocated_tcb_index;
+
+	return &sTask_list[sCurrent_tcb_index];
+}
+
+static KernelTcb_t* Scheduler_priority_algorithm(void)
+{
+	KernelTcb_t* sCurrent = &list[sCurrent_tcb_index];
+	for(uint32_t i=0; i<sAllocated_tcb_index; i++)
+	{
+		KernelTcb_t* pNextTcb = &sTask_list[i];
+		if(pNextTcb != sCurrent)
+		{
+			if(pNextTcb->priority <= sCurrent->priority)
+			{
+				return pNextTcb;
+			}
+		}
+	}
+	return sCurrent;
+}
+
